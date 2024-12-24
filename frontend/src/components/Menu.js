b42fce431afaa4026b17fdd1edcd4bf75198437a
denis.popov <denispopov2112@gmail.com>
2024-12-24 08:39:03 +0100

    Sign In / Sign Up

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/auth';
 
 const Menu = () => {
     const { user, logout } = useAuth();
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-    const handleLogout = async () => {
-        await logout();
+    const toggleDropdown = () => {
+        setDropdownOpen((prev) => !prev);
+    };
+
+    const handleLogout = () => {
+        logout();
     };
 
     return (
@@ -18,33 +23,54 @@ const Menu = () => {
                     </Link>
                 </li>
                 {user ? (
-                    <>
-                        <li className="text-white">Welcome, {user?.firstname} {user?.lastname}</li>
-                        <li>
-                            <Link to="/my-account" className="text-white no-underline hover:underline">
-                                My Account
-                            </Link>
-                        </li>
-                        <li>
-                            <button
-                                onClick={handleLogout}
-                                className="text-white no-underline hover:underline bg-transparent border-none cursor-pointer"
-                            >
-                                Log Out
-                            </button>
-                        </li>
-                    </>
+                    <li className="relative">
+                        <button
+                            onClick={toggleDropdown}
+                            className="text-white no-underline hover:underline bg-transparent border-none cursor-pointer"
+                        >
+                            {`Welcome, ${user.firstname} ${user.lastname}`}
+                        </button>
+                        {dropdownOpen && (
+                            <ul className="absolute right-0 bg-white text-black mt-2 py-2 w-48 shadow-lg rounded">
+                                <li className="px-4 py-2 hover:bg-gray-200">
+                                    <Link to="/my-account" className="no-underline text-black">
+                                        My Account
+                                    </Link>
+                                </li>
+                                <li className="px-4 py-2 hover:bg-gray-200">
+                                    <button
+                                        onClick={handleLogout}
+                                        className="w-full text-left bg-transparent border-none cursor-pointer"
+                                    >
+                                        Log Out
+                                    </button>
+                                </li>
+                            </ul>
+                        )}
+                    </li>
                 ) : (
                     <>
-                        <li>
-                            <Link to="/signin" className="text-white no-underline hover:underline">
-                                Sign In
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/signup" className="text-white no-underline hover:underline">
-                                Sign Up
-                            </Link>
+                        <li className="relative">
+                            <button
+                                onClick={toggleDropdown}
+                                className="text-white no-underline hover:underline bg-transparent border-none cursor-pointer"
+                            >
+                                {`Welcome, Guest`}
+                            </button>
+                            {dropdownOpen && (
+                                <ul className="absolute right-0 bg-white text-black mt-2 py-2 w-48 shadow-lg rounded">
+                                    <li className="px-4 py-2 hover:bg-gray-200">
+                                        <Link to="/signin" className="no-underline text-black">
+                                            Sign In
+                                        </Link>
+                                    </li>
+                                    <li className="px-4 py-2 hover:bg-gray-200">
+                                        <Link to="/signup" className="no-underline text-black">
+                                            Sign Up
+                                        </Link>
+                                    </li>
+                                </ul>
+                            )}
                         </li>
                     </>
                 )}
